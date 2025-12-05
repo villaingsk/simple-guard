ec7565fe0847758be11560725223c20c57968990
Kref Studio <33785308+justyupi@users.noreply.github.com>
2025-12-05 11:23:05 +0700

    Update helpers.php

--- includes/helpers.php
+++ includes/helpers.php
@@ -2,8 +2,8 @@
 if (!defined('ABSPATH')) exit;
 
 
-if (!function_exists('wpgp_get_client_ip')) {
-    function wpgp_get_client_ip(){
+if (!function_exists('sg_get_client_ip')) {
+    function sg_get_client_ip(){
         if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
             $list = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
             return trim($list[0]);
@@ -14,9 +14,9 @@ if (!function_exists('wpgp_get_client_ip')) {
 }
 
 
-if (!function_exists('wpgp_is_ip_whitelisted')) {
-    function wpgp_is_ip_whitelisted($ip){
-        $opts = get_option('wpgp_options', []);
+if (!function_exists('sg_is_ip_whitelisted')) {
+    function sg_is_ip_whitelisted($ip){
+        $opts = get_option('sg_options', []);
         $list = explode("\n", ($opts['whitelist_ips'] ?? ''));
         $list = array_map('trim', $list);
         $list = array_filter($list);
